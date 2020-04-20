a622ffb9d80479b5be234a0cd1a5553284201a6e
missTinuade <estherjolugba@yahoo.com>
2020-04-20 19:45:52 +0000

    Initial commit

---
+++ app/src/main/java/com/tinuade/porfolio/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.tinuade.porfolio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
