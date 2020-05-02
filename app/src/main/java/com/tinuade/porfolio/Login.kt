044d9dea878227b46faaf03de8fc8261aa0ddac5
missTinuade <estherjolugba@yahoo.com>
2020-05-02 21:12:28 +0000

    login and Sign up screen done

---
+++ app/src/main/java/com/tinuade/porfolio/Login.kt
@@ -0,0 +1,19 @@
+package com.tinuade.porfolio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        signUpLink.setOnClickListener {
+            val intent=Intent(this,SignUp::class.java)
+            startActivity(intent)
+        }
+    }
+}
