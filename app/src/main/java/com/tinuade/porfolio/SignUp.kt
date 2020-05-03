e5c61d5edc1404d52eb2f2ceccd9fa53b15dfc87
missTinuade <estherjolugba@yahoo.com>
2020-05-03 12:48:46 +0000

    crashing bug fixed

--- app/src/main/java/com/tinuade/porfolio/SignUp.kt
+++ app/src/main/java/com/tinuade/porfolio/SignUp.kt
@@ -8,26 +8,20 @@ import android.text.TextUtils
 import android.util.Patterns
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.tinuade.porfolio.Login.AppPreferences.editor
-import com.tinuade.porfolio.Login.AppPreferences.preferences
 import com.tinuade.porfolio.SignUp.AppPreferences.MODE
 import com.tinuade.porfolio.SignUp.AppPreferences.NAME
-//import com.tinuade.porfolio.SignUp.AppPreferences.editor
-//import com.tinuade.porfolio.SignUp.AppPreferences.preferences
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUp : AppCompatActivity() {
     object AppPreferences {
         const val NAME = "com.tinuade.porfolio.portfolio"
         const val MODE = Context.MODE_PRIVATE
-        //lateinit var preferences: SharedPreferences
-        //lateinit var editor: SharedPreferences.Editor
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        progress.visibility=View.GONE
+        progress.visibility = View.GONE
 
         signInLink.setOnClickListener {
             val intent = Intent(this, Login::class.java)
@@ -45,7 +39,6 @@ class SignUp : AppCompatActivity() {
         val name: String = _fullname_editText.text.toString()
         val email: String = email_editText.text.toString()
         val password: String = password_editText.text.toString()
-        val confirmPassword: String = confirm_password_editText.text.toString()
         val phoneNumber: String = phone_number_editText.text.toString()
 
         if (TextUtils.isEmpty(name) && name.length < 3) {
@@ -58,30 +51,28 @@ class SignUp : AppCompatActivity() {
             showToast("Please Enter a valid Password and Email Address")
         } else if (TextUtils.isEmpty(password) && !email.contains("@")) {
             showToast("Please Enter a valid Password and email address")
-        } else if (password !== (confirmPassword)) {
-            showToast("Passwords do not match")
-            password_editText.setText("")
-            confirm_password_editText.setText("")
         } else if (TextUtils.isEmpty(phoneNumber) && phoneNumber.length < 9) {
             showToast("Please Enter a valid Phone Number")
-        } else{
+            password_editText.setText("")
+        } else {
 
-            val preferences:SharedPreferences =getSharedPreferences(NAME, MODE)
+            val preferences: SharedPreferences = getSharedPreferences(NAME, MODE)
 
-        // as now we have information in string. Lets stored them with the help of editor
-         val editor: SharedPreferences.Editor =preferences.edit()
-            editor.putString(email + password + "data", name + "\n" + email)
-        editor.apply()   // commit the values
-        progress.visibility=View.VISIBLE
-        // after saving the value open next activity
-        val intent = Intent(this, Login::class.java)
-        startActivity(intent)
+            // as now we have information in string. Lets stored them with the help of editor
+            val editor: SharedPreferences.Editor = preferences.edit()
+            editor.putString(email + password + "data", "Email: $email")
+            editor.apply()   // commit the values
+            progress.visibility = View.VISIBLE
+            // after saving the value open next activity
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
 
-        showToast("Successful")
-        progress.visibility=View.GONE
+            showToast("Successful")
+            progress.visibility = View.GONE
 
+        }
     }
-}}
+}
 
 
 
